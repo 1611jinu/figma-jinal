caa449613c004c4ebf65252394bd9aa1f5714f19
Jinu <ravaljinal16@gmail.com>
2024-01-21 20:25:11 +0530

    test

---
+++ MyStyleD/build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Sun, 21 Jan 2024 14:34:18 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
---
+++ MyStyleD/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Sun, 21 Jan 2024 14:34:18 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
